9c2d0c10b0636a9d4251307264a0c7ce61fa1d29
Ole-Paul Heinzelmann <oheinzel@1-B-2.42heilbronn.de>
2023-01-04 10:43:40 +0100

    2023-01-04 10:43:40

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 13:13:10 by oheinzel          #+#    #+#             */
-/*   Updated: 2023/01/04 09:26:29 by oheinzel         ###   ########.fr       */
+/*   Updated: 2023/01/04 10:42:25 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,15 @@ void	free_str_arr(char **ptr)
 		free(ptr[i]);
 	free(ptr);
 	ptr = NULL;
+}
+
+void	change_src(char *file, int src)
+{
+	int	fd;
+
+	fd = open(file, 0);
+	dup2(fd, src);
+	close(fd);
 }
 
 char	*get_path(char **env, char *arg)
@@ -46,8 +55,37 @@ char	*get_path(char **env, char *arg)
 	return (free(paths), NULL);
 }
 
+void	exec_cmd(char *arg, char **env)
+{
+	char	**cmd;
+
+	cmd = ft_split(arg, ' ');
+	execve(get_path(env, cmd[0]), cmd, env);
+	free_str_arr(cmd);
+}
+
 int	main(int argc, char *argv[], char *env[])
 {
+	int		src[2];
+	pid_t	child;
+
 	if (argc != 5)
-		return (ft_putendl_fd("ERROR: Too few arguments", 2), 0);
+		return (ft_putendl_fd("ERROR: Wrong arguments count", 2), 0);
+	change_src(argv[1], 1);
+	child = fork();
+	pipe(src);
+	if (child == 0)
+	{
+		dup2(src[1], 1);
+		close(src[0]);
+		exec_cmd(argv[4], env);
+		change_src(argv[5], 0);
+	}
+	else
+	{
+		dup2(src[0], 0);
+		close(src[1]);
+		exec_cmd(argv[3], env);
+	}
+	return (0);
 }
