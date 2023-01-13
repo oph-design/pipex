bed875dc63ce361b2d8bcd149d26caa91f6f229b
oph-design <115570424+oph-design@users.noreply.github.com>
2023-01-13 19:38:48 +0100

    Update pipex_bonus.h

--- include/pipex_bonus.h
+++ include/pipex_bonus.h
@@ -21,7 +21,6 @@
 void	ft_error(char *mess, int exit_code);
 char	*format_cmd(char *cmd);
 char	**join_cmd(char **str);
-int		here_doc(char **argv);
-int		change_src(char *file, int src);
+void		here_doc(char **argv);
 
 #endif
